64d168d4a400bde737ad57f5f67fde7a5cb52221
Tsamara Esperanti Erwin <sarah.esperanti@gmail.com>
2021-11-20 23:38:46 +0700

    change ssl

--- src/models/index.js
+++ src/models/index.js
@@ -3,8 +3,10 @@ const dbConfig = require("../config/dbConfig");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
-    dialect: dbConfig.dialect
-  })
+    dialect: {
+      ssl: true
+    }
+})
 
 const db = {}
 
